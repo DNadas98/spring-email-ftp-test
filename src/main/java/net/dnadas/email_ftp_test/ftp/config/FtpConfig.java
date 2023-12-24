05f0fa38eece92ecda410c76c26a617324761ac3
DNadas98 <daniel.nadas.123@gmail.com>
2023-12-24 17:54:17 +0100

    upgrade config to FTPS, set Abstract version of the session factory at the service

--- src/main/java/net/dnadas/email_ftp_test/ftp/config/FtpConfig.java
+++ src/main/java/net/dnadas/email_ftp_test/ftp/config/FtpConfig.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.ftp.session.DefaultFtpSessionFactory;
+import org.springframework.integration.ftp.session.DefaultFtpsSessionFactory;
 
 @Configuration
 public class FtpConfig {
@@ -15,21 +15,35 @@ public class FtpConfig {
   private final Integer ftpPort;
   private final String ftpUsername;
   private final String ftpPassword;
+  private final Boolean useClientSSLAuth;
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
   @Autowired
   public FtpConfig(
     @Value("${FTP_HOST}") String ftpHost, @Value("${FTP_PORT}") Integer ftpPort,
-    @Value("${FTP_USERNAME}") String ftpUsername, @Value("${FTP_PASSWORD}") String ftpPassword) {
+    @Value("${FTP_USERNAME}") String ftpUsername, @Value("${FTP_PASSWORD}") String ftpPassword,
+    @Value("${FTP_USE_CLIENT_SSL_AUTH}") Boolean useClientSSLAuth) {
     this.ftpHost = ftpHost;
     this.ftpPort = ftpPort;
     this.ftpUsername = ftpUsername;
     this.ftpPassword = ftpPassword;
+    this.useClientSSLAuth = useClientSSLAuth;
   }
 
   @Bean
-  public DefaultFtpSessionFactory sftpSessionFactory() {
-    DefaultFtpSessionFactory factory = new DefaultFtpSessionFactory();
+  public DefaultFtpsSessionFactory ftpsSessionFactory() {
+    DefaultFtpsSessionFactory factory = new DefaultFtpsSessionFactory();
+    setDefaultFtpSettings(factory);
+    if (useClientSSLAuth) {
+      factory.setNeedClientAuth(true);
+    }
+    logger.info(String.format(
+      "Configuring FTPS - Host: %s, Port: %d, Username: %s, Client-side SSL authentication: %s",
+      ftpHost, ftpPort, ftpUsername, useClientSSLAuth ? "Enabled" : "Disabled"));
+    return factory;
+  }
+
+  private void setDefaultFtpSettings(DefaultFtpsSessionFactory factory) {
     factory.setHost(ftpHost);
     factory.setPort(ftpPort);
     factory.setControlEncoding("UTF-8");
@@ -37,9 +51,5 @@ public class FtpConfig {
     factory.setPassword(ftpPassword);
     factory.setClientMode(FTPClient.PASSIVE_LOCAL_DATA_CONNECTION_MODE);
     factory.setConnectTimeout(60000);
-    logger.info(
-      String.format("Configuring FTP - host: %s, port: %d, username: %s", ftpHost, ftpPort,
-        ftpUsername));
-    return factory;
   }
 }
